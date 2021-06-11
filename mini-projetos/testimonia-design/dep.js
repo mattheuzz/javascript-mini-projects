4150f482b11c96231e238e31aa1f146ba83b01f9
Matheus Henrique <mattheus.h.m.s@gmail.com>
2021-06-11 17:58:20 -0300

    mini projeto 2
    falta conseguir mudar o bg

---
+++ mini-projetos/testimonia-design/dep.js
@@ -0,0 +1,35 @@
+function bt1(){
+    var txt = document.querySelector('.txt')
+    var nome = document.querySelector('.nome')
+    txt.innerHTML = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda provident totam tempore, iure quas ex nesciunt consectetur, facilis possimus obcaecati voluptatem repellat placeat sunt! Maxime consequatur nesciunt voluptas blanditiis error.'
+    nome.innerHTML = 'Matheus'
+}
+
+function bt2(){
+    var txt = document.querySelector('.txt')
+    var nome = document.querySelector('.nome')
+    txt.innerHTML = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis temporibus tenetur dicta natus voluptatem, debitis itaque ipsam quod aliquam, porro exercitationem. Molestias hic aspernatur nihil, nostrum explicabo incidunt sunt cupiditate!'
+    nome.innerHTML = 'Caio'
+}
+
+function bt3(){
+    var txt = document.querySelector('.txt')
+    var nome = document.querySelector('.nome')
+    txt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima deleniti placeat id, sed assumenda soluta? Est, culpa fugit sunt aliquid voluptatum repellendus ipsam neque obcaecati, aut quaerat quibusdam sint quos.'
+    nome.innerHTML = 'Yumi'
+}
+
+function bt4(){
+    var txt = document.querySelector('.txt')
+    var nome = document.querySelector('.nome')
+    txt.innerHTML = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi enim numquam in ipsum veritatis! Perspiciatis quisquam provident, incidunt, quidem voluptatibus molestias blanditiis veritatis tempora quam ea repellat praesentium alias? Maxime?'
+    nome.innerHTML = 'Tamires'
+    document.getElementsByClassName('.bloco').style.backgroundColor = 'blue'
+}
+
+function bt5(){
+    var txt = document.querySelector('.txt')
+    var nome = document.querySelector('.nome')
+    txt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum eveniet magnam ab quaerat laboriosam inventore, est, similique, necessitatibus reiciendis sequi et harum. Magni, voluptates? Totam fugit eaque distinctio! Voluptates, maiores?'
+    nome.innerHTML = 'Bruno'
+}
