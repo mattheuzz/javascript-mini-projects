dba914c2effc724a516ad1e18a586926f3d96f9f
Matheus Henrique <mattheus.h.m.s@gmail.com>
2021-06-13 14:01:00 -0300

    depoimento finalizado

--- mini-projetos/testimonia-design/dep.js
+++ mini-projetos/testimonia-design/dep.js
@@ -1,35 +1,46 @@
 function bt1(){
     var txt = document.querySelector('.txt')
     var nome = document.querySelector('.nome')
+    var bloco = document.querySelector('.bloco')
     txt.innerHTML = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda provident totam tempore, iure quas ex nesciunt consectetur, facilis possimus obcaecati voluptatem repellat placeat sunt! Maxime consequatur nesciunt voluptas blanditiis error.'
     nome.innerHTML = 'Matheus'
+    bloco.style.backgroundColor = 'darkcyan'
+    
 }
 
 function bt2(){
+    var bloco = document.querySelector('.bloco')
     var txt = document.querySelector('.txt')
     var nome = document.querySelector('.nome')
     txt.innerHTML = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis temporibus tenetur dicta natus voluptatem, debitis itaque ipsam quod aliquam, porro exercitationem. Molestias hic aspernatur nihil, nostrum explicabo incidunt sunt cupiditate!'
     nome.innerHTML = 'Caio'
+    bloco.style.backgroundColor = 'red'
 }
 
+
 function bt3(){
+    var bloco = document.querySelector('.bloco')
     var txt = document.querySelector('.txt')
     var nome = document.querySelector('.nome')
     txt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima deleniti placeat id, sed assumenda soluta? Est, culpa fugit sunt aliquid voluptatum repellendus ipsam neque obcaecati, aut quaerat quibusdam sint quos.'
     nome.innerHTML = 'Yumi'
+    bloco.style.backgroundColor = 'pink'
 }
 
 function bt4(){
+    var bloco = document.querySelector('.bloco')
     var txt = document.querySelector('.txt')
     var nome = document.querySelector('.nome')
     txt.innerHTML = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi enim numquam in ipsum veritatis! Perspiciatis quisquam provident, incidunt, quidem voluptatibus molestias blanditiis veritatis tempora quam ea repellat praesentium alias? Maxime?'
     nome.innerHTML = 'Tamires'
-    document.getElementsByClassName('.bloco').style.backgroundColor = 'blue'
+    bloco.style.backgroundColor = 'green'
 }
 
 function bt5(){
+    var bloco = document.querySelector('.bloco')
     var txt = document.querySelector('.txt')
     var nome = document.querySelector('.nome')
     txt.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum eveniet magnam ab quaerat laboriosam inventore, est, similique, necessitatibus reiciendis sequi et harum. Magni, voluptates? Totam fugit eaque distinctio! Voluptates, maiores?'
     nome.innerHTML = 'Bruno'
+    bloco.style.backgroundColor = 'grey'
 }
